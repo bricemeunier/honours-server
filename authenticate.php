f383b89487b1641258ee5ef46bea0a0cf2a92e3c
Meunier Brice <brice.meunier@etu.unilim.fr>
2020-03-09 17:11:20 +0000

    test

--- authenticate.php
+++ authenticate.php
@@ -9,7 +9,7 @@ $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	die ();
-	header('Location: index.html?error=0');
+	header('Location: index.html?error=0'); 
 }
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
