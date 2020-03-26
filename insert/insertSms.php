2b30fa772b555fd077e2f2654d32dab0964f9983
EC2 Default User <ec2-user@ip-172-31-16-115.eu-west-2.compute.internal>
2020-03-26 15:40:33 +0000

    sms sent now retrieved as well as sms received

--- insert/insertSms.php
+++ insert/insertSms.php
@@ -1,14 +1,16 @@
 <?php
 
 include '../DatabaseConfig.php' ;
-
+ 
  $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
  $key = $_POST['key'];
  $address = $_POST['address'];
  $message = $_POST['message'];
  $date = $_POST['date'];
- $action = $_POST['action'];
- $Sql_Query = "insert into sms (idUser,date,action,address,message) values ('$key','$date','$action','$address','$message')";
-
- mysqli_query($con,$Sql_Query);
+ $action=$_POST['action'];
+ if ($stmt = $con->prepare('INSERT INTO sms (idUser,date,action,address,message) values (?,?,?,?,?)')){
+ 	$stmt->bind_param('sssss',$key,$date,$action,$address,$message);
+ 	$stmt->execute();
+	$stmt->close();
+ }
 ?>
