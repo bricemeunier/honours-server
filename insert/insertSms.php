4cf97e0b6c7395ad05c9df23e1d4f951db4eee21
brice meunier <brice.meunier@etu.unilim.fr>
2020-04-16 14:46:40 +0200

    detecting bad word nuances

--- insert/insertSms.php
+++ insert/insertSms.php
@@ -9,12 +9,30 @@ $spam_words = file('../ressource/bad-words.txt', FILE_IGNORE_NEW_LINES);
 $warning="0";
 
 function contains($str, array $arr) {
-    foreach($arr as $a) {
-        if (stripos($str,$a) !== false) {
-          return true;
+  foreach($arr as $a) {
+      if (stripos($str,$a) !== false) {
+
+        $index=stripos($str,$a)+strlen($a);
+        if (stripos($str,$a)!=0) {
+          if (ctype_alpha($str[stripos($str,$a)-1]))  return false;
         }
-    }
-    return false;
+
+        if (isset($str[$index])) {
+          if (ctype_alpha($str[$index])){
+            if (isset($str[$index+2])) {
+              return substr($str,$index,3)=="ing" OR substr($str,$index,2)=="ed" OR substr($str,$index,2)=="s " OR substr($str,$index,2)=="es";
+            }
+            elseif (isset($str[$index+1])) {
+              return substr($str,$index,2)=="ed" OR substr($str,$index,2)=="s " OR substr($str,$index,2)=="es";
+            }
+            else return substr($str,$index,1)=="s";
+          }
+          else return true;
+        }
+        else return true;
+      }
+  }
+  return false;
 }
 
 $key = $_POST['key'];
